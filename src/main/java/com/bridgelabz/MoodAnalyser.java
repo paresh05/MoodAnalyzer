08be2b9caf047e44875d5a706707e2d93160b04d
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-09-12 12:45:55 +0530

    Added: Method to analyze mood with the message passed

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -10,8 +10,8 @@ public class MoodAnalyser {
 	 * This function is used to analyze the mood according to the message passed
 	 * 
 	 * If the message passed contains Sad then it returns SAD else returns HAPPY
-	 * 
 	 */
+	
 	public String analyseMood(String message)
 	{
 		if(message.contains("Sad"))
