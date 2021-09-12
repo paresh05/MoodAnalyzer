b2177bc0a3e9e03543c44ffe5f6f3d012587cfbd
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-09-12 14:15:14 +0530

    Refactor: Test Method to pass message in constructor

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -7,16 +7,15 @@ public class MoodAnalyserTest {
 
 	@Test
 	public void givenMessageWhenSad_ShouldReturnSad() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser();
-		String mood = moodAnalyser.analyseMood("This is a Sad message");
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I'm in Sad Mood");
+		String mood = moodAnalyser.analyseMood();
 		Assert.assertEquals("SAD", mood);
 	}
 	
-
 	@Test
 	public void givenMessageWhenHappy_ShouldReturnHappy() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser();
-		String mood = moodAnalyser.analyseMood("This is a Happy message");
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I'm in Happy Mood");
+		String mood = moodAnalyser.analyseMood();
 		Assert.assertEquals("HAPPY", mood);
 	}
 
