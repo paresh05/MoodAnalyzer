83fb9676ad6ac65e38777ea8e383415cea046dac
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-09-12 15:48:05 +0530

    Added: Custom Exception

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -2,28 +2,53 @@ package com.bridgelabz;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class MoodAnalyserTest {
 
 	@Test
 	public void givenMessageWhenSad_ShouldReturnSad() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser("I'm in Sad Mood");
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("SAD", mood);
+		String mood;
+		try {
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(MoodAnalyserException.class);
+			mood = moodAnalyser.analyseMood();
+			Assert.assertEquals("SAD", mood);
+		} catch (MoodAnalyserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
 	public void givenMessageWhenHappy_ShouldReturnHappy() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser("I'm in Happy Mood");
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("HAPPY", mood);
+		String mood;
+		try {
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(MoodAnalyserException.class);
+			mood = moodAnalyser.analyseMood();
+			Assert.assertEquals("HAPPY", mood);
+		} catch (MoodAnalyserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
 	public void givenMessageWhenNull_ShouldReturnHappy() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("HAPPY", mood);
+		String mood;
+		try {
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(MoodAnalyserException.class);
+			mood = moodAnalyser.analyseMood();
+			Assert.assertEquals("HAPPY", mood);
+		} catch (MoodAnalyserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
