e301a195987efb30b07435a88fcfdde09ef698a8
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-09-12 14:12:04 +0530

    Refactor: To take mood message in constructor

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -6,13 +6,25 @@ package com.bridgelabz;
  */
 public class MoodAnalyser {
 	
+	String message;
+
+	//Parameterized Constructor
+	public MoodAnalyser(String message) {
+		super();
+		this.message = message;
+	}
+	//Default Constructor
+	public MoodAnalyser() {
+		
+	}
+	
 	/**
 	 * This function is used to analyze the mood according to the message passed
 	 * 
 	 * If the message passed contains Sad then it returns SAD else returns HAPPY
 	 */
 	
-	public String analyseMood(String message)
+	public String analyseMood()
 	{
 		if(message.contains("Sad"))
 			return "SAD";
